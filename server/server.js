8d928d221fa95aaad252ccd11e7a74431870068a
Reza De <rezade2004@gmail.com>
2018-08-25 20:12:42 +0430

    added start

--- server/server.js
+++ server/server.js
@@ -53,8 +53,8 @@ app.get('/user/profile', auth, (req,res)=>{
     res.status(200).send(req.token);
 });
 
-
 const port = process.env.PORT || 3000;
+
 app.listen(port,()=>{
    console.log(`Started on Port ${port}`);
 });
